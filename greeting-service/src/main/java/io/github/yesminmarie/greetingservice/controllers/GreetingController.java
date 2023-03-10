a24d6c0d0d489d2795777f48720428a6b4038049
Yesmin Lahoud <ymslahoud@gmail.com>
2023-03-10 11:56:46 -0300

    greeting service e config server

---
+++ greeting-service/src/main/java/io/github/yesminmarie/greetingservice/controllers/GreetingController.java
@@ -0,0 +1,35 @@
+package io.github.yesminmarie.greetingservice.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import io.github.yesminmarie.greetingservice.configuration.GreetingConfiguration;
+import io.github.yesminmarie.greetingservice.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "%s, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@Autowired
+	private GreetingConfiguration configuration;
+	
+	@RequestMapping("/greeting")
+	public Greeting greeting(
+			@RequestParam(value="name",
+			defaultValue = "") String name) {
+
+		if(name.isEmpty()){
+			name = configuration.getDefaultValue();
+		}
+
+		return new Greeting(
+					counter.incrementAndGet(),
+					String.format(template, configuration.getGreeting(), name)
+				);
+	}
+}
