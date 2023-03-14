d6396d81710e4d5a8269ddde521df0bf49a8bb3c
Yesmin Lahoud <ymslahoud@gmail.com>
2023-03-14 16:12:14 -0300

    load balancing

--- book-service/src/main/java/io/github/yesminmarie/bookservice/controller/BookController.java
+++ book-service/src/main/java/io/github/yesminmarie/bookservice/controller/BookController.java
@@ -39,7 +39,7 @@ public class BookController {
         var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
 
         var port = environment.getProperty("local.server.port");
-        book.setEnvironment(port + " FEIGN");
+        book.setEnvironment("Port: " + port + " Cambio: " + cambio.getEnvironment());
         book.setPrice(cambio.getConvertedValue());
         return book;
     }
