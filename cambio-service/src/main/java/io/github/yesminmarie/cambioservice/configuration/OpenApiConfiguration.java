1c0e058c145dcbde27b361a9dbebc9213d5d842f
Yesmin Lahoud <ymslahoud@gmail.com>
2023-03-15 18:14:48 -0300

    OpenAPI UI

--- cambio-service/src/main/java/io/github/yesminmarie/cambioservice/configuration/OpenApiConfiguration.java
+++ cambio-service/src/main/java/io/github/yesminmarie/cambioservice/configuration/OpenApiConfiguration.java
@@ -1,17 +1,16 @@
-package io.github.yesminmarie.bookservice.configuration;
-
-import org.springframework.context.annotation.Bean;
+package io.github.yesminmarie.cambioservice.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
 
 @OpenAPIDefinition(info =
-@Info(title = "Book Service API",
+@Info(title = "Cambio Service API",
         version = "v1",
-        description = "Documentation of Book Service API"))
+        description = "Documentation of Cambio Service API"))
 public class OpenApiConfiguration {
 
     @Bean
@@ -20,7 +19,7 @@ public class OpenApiConfiguration {
                 .components(new Components())
                 .info(
                         new io.swagger.v3.oas.models.info.Info()
-                                .title("Book Service API")
+                                .title("Cambio Service API")
                                 .version("v1")
                                 .license(
                                         new License()
--- cambio-service/src/main/java/io/github/yesminmarie/cambioservice/controller/CambioController.java
+++ cambio-service/src/main/java/io/github/yesminmarie/cambioservice/controller/CambioController.java
@@ -2,6 +2,8 @@ package io.github.yesminmarie.cambioservice.controller;
 
 import io.github.yesminmarie.cambioservice.model.Cambio;
 import io.github.yesminmarie.cambioservice.repository.CambioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+@Tag(name = "Cambio Service API")
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
@@ -22,6 +25,7 @@ public class CambioController {
     @Autowired
     private CambioRepository repository;
 
+    @Operation(description = "Get cambio from currency")
     @GetMapping(value = "/{amount}/{from}/{to}")
     public Cambio getCambio(
             @PathVariable("amount")BigDecimal amount,
