c13a8c713a313c3d7d45150e1107891bc898b69e
Yesmin Lahoud <ymslahoud@gmail.com>
2023-03-15 15:49:32 -0300

    retry

---
+++ book-service/src/main/java/io/github/yesminmarie/bookservice/controller/FooBarController.java
@@ -0,0 +1,26 @@
+package io.github.yesminmarie.bookservice.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("book-service")
+public class FooBarController {
+
+    private Logger logger = LoggerFactory.getLogger(FooBarController.class);
+
+    @GetMapping("/foo-bar")
+    @Retry(name = "foo-bar")
+    public String fooBar(){
+        logger.info("Request to foo-bar is received!");
+        var response = new RestTemplate()
+                .getForEntity("http://localhost:8080/foo-bar", String.class);
+        //return "Foo-Bar!!!";
+        return response.getBody();
+    }
+}
