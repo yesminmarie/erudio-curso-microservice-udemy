0dc2a7500eebf954ded6f8d0e5412dce4d32ff50
Yesmin Lahoud <ymslahoud@gmail.com>
2023-03-15 16:38:23 -0300

    Circuit Breaker

--- book-service/src/main/java/io/github/yesminmarie/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/io/github/yesminmarie/bookservice/controller/FooBarController.java
@@ -1,5 +1,6 @@
 package io.github.yesminmarie.bookservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +16,8 @@ public class FooBarController {
     private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
     @GetMapping("/foo-bar")
-    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+    //@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+    @CircuitBreaker(name = "foo-bar", fallbackMethod = "fallbackMethod")
     public String fooBar(){
         logger.info("Request to foo-bar is received!");
         var response = new RestTemplate()
