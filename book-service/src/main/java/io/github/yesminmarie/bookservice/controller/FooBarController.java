b88d0c947efb3e084f76425d546c864c294ba229
Yesmin Lahoud <ymslahoud@gmail.com>
2023-03-15 17:08:55 -0300

    RateLimiter e Bulkhead

--- book-service/src/main/java/io/github/yesminmarie/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/io/github/yesminmarie/bookservice/controller/FooBarController.java
@@ -1,6 +1,8 @@
 package io.github.yesminmarie.bookservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,13 +19,15 @@ public class FooBarController {
 
     @GetMapping("/foo-bar")
     //@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
-    @CircuitBreaker(name = "foo-bar", fallbackMethod = "fallbackMethod")
+    //@CircuitBreaker(name = "foo-bar", fallbackMethod = "fallbackMethod")
+    //@RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public String fooBar(){
         logger.info("Request to foo-bar is received!");
-        var response = new RestTemplate()
-                .getForEntity("http://localhost:8080/foo-bar", String.class);
-        //return "Foo-Bar!!!";
-        return response.getBody();
+//        var response = new RestTemplate()
+//                .getForEntity("http://localhost:8080/foo-bar", String.class);
+        return "Foo-Bar!!!";
+        //return response.getBody();
     }
 
     public String fallbackMethod(Exception ex) {
